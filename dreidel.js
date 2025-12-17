677eb68b69738486b6d2e5dafa6fa902d27e0342
James Hawker <526rserbinenko@frhsd.com>
2025-12-17 23:45:09 +0000

    MY DREIDEL IS HERE!

--- dreidel.js
+++ dreidel.js
@@ -1,22 +1,31 @@
 let shape;
 let bg;
+let img;
 let decrease = 1;
 let currentAngle = 0;
 let turning = true;
 const threshold = 20;
 const angleThreshold = 0.2;
 function preload() {
-  shape = loadModel("Dreidel.stl", true);
+  shape = loadModel("dreidel.obj", true);
   bg = loadImage("cayley_interior.jpg");
+  img = loadImage("Dreidel.png");
 }
 function setup() {
   createCanvas(400, 400, WEBGL);
   describe("A dreidel");
+  window.addEventListener("click", () => {
+    turning = true;
+    decrease = 1;
+    currentAngle = 0;
+  });
 }
 function draw() {
   background(255);
   noStroke();
-  const c = color(82, 162, 255);
+  rotateX(PI);
+  scale(-1, 1);
+  const c = color(255);
   const north = createVector(-1, 0, -0.3).normalize();
   directionalLight(c, north.x, north.y, north.z);
   const east = createVector(0, 1, -0.3).normalize();
@@ -25,7 +34,7 @@ function draw() {
   directionalLight(c, west.x, west.y, west.z);
   const south = createVector(1, 0, -0.3).normalize();
   directionalLight(c, south.x, south.y, south.z);
-  ambientMaterial(82, 162, 255);
+  texture(img);
   if (turning) {
     currentAngle += 0.2 * decrease;
     decrease *= 0.99;
