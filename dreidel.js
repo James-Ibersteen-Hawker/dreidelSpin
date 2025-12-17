2bcca1975b9cea006d74a162143ddbd881a36262
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-12-17 11:55:47 -0500

    decelerating dreidel

--- dreidel.js
+++ dreidel.js
@@ -1,5 +1,10 @@
 let shape;
 let bg;
+let decrease = 1;
+let currentAngle = 0;
+let turning = true;
+const threshold = 20;
+const angleThreshold = 0.2;
 function preload() {
   shape = loadModel("Dreidel.stl", true);
   bg = loadImage("cayley_interior.jpg");
@@ -10,15 +15,23 @@ function setup() {
 }
 function draw() {
   background(255);
-  fill(82, 162, 255);
-  orbitControl();
   noStroke();
-  const c = color(255, 255, 255);
-  directionalLight(c, -1, -1, -0.5);
-  directionalLight(c, 1, -1, -0.5);
-  directionalLight(c, 0, 1, 0.5);
-  ambientMaterial(255, 255, 255);
-  specularMaterial(255, 255, 255);
-  shininess(200);
+  const c = color(82, 162, 255);
+  const north = createVector(-1, 0, -0.3).normalize();
+  directionalLight(c, north.x, north.y, north.z);
+  const east = createVector(0, 1, -0.3).normalize();
+  directionalLight(c, east.x, east.y, east.z);
+  const west = createVector(0, -1, -0.3).normalize();
+  directionalLight(c, west.x, west.y, west.z);
+  const south = createVector(1, 0, -0.3).normalize();
+  directionalLight(c, south.x, south.y, south.z);
+  ambientMaterial(82, 162, 255);
+  if (turning) {
+    currentAngle += 0.2 * decrease;
+    decrease *= 0.99;
+    rotateY(currentAngle);
+    if (Math.abs(currentAngle - threshold) < angleThreshold) turning = false;
+    console.log(currentAngle);
+  } else rotateY(currentAngle);
   model(shape);
 }
